d348eaffe07ab8a33ff18dd1f8939b02be9fdd24
JackelinEspinosa <134608173+JackelinEspinosa@users.noreply.github.com>
2023-10-24 12:22:11 -0500

    Delete Sesion-8 directory

--- Sesion-8/Postwork/src/main/java/com/example/postwork/persistence/PersonaRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.postwork.persistence;
-
-import com.example.postwork.model.Persona;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PersonaRepository extends JpaRepository<Persona, Long> {
-}
